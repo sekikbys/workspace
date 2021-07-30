1d2244a5f2ed0a555df076aeecaa47b9187c9d0f
sekikbys <sekikbys@yahoo.co.jp>
2021-07-30 16:51:30 +0900

    Initial commit

---
+++ src/main/java/com/demo01/demo01Application.java
@@ -0,0 +1,12 @@
+package com.demo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class demo01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(demo01Application.class,args);
+    }
+}
